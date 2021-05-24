5d89058c2d8515ed17b6d107a3350adc6259bb86
DDor-H <DDor_H@163.com>
2021-05-24 22:04:25 +0800

    进度条显示

---
+++ day03/process.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+	char buf[100] = {};
+	char *p = "|/-\\";
+
+	int i;
+	for (i=0;i<100;++i)
+	{
+		buf[i] = '#';
+		printf("[%-100s][%d%%][%c]\r", buf, i+1, p[i%4]);
+		fflush(stdout);
+		usleep(500000);
+	}
+	return 0;
+}
